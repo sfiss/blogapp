25294c9121592ef39f1ffcfdfcc9fa1ef9c68a02
Sebastian Fiss <sebastianfiss@gmail.com>
2016-08-05 16:29:03 +0200

    stash changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import {NGB_DIRECTIVES, NGB_PRECOMPILE} from '@ng-bootstrap/ng-bootstrap';
+import {NGB_DIRECTIVES, NGB_PRECOMPILE, NGB_COLLAPSE_DIRECTIVES } from '@ng-bootstrap/ng-bootstrap';
+
+export class NavController {
+	collapsed: boolean;
+}
 
 @Component({
 	selector: 'blog-app',
 	precompile: [ NGB_PRECOMPILE ],
-	directives: [ NGB_DIRECTIVES ],
+	directives: [ NGB_DIRECTIVES, NGB_COLLAPSE_DIRECTIVES ],
 	styleUrls: ['app/app.styles.css'],
 	templateUrl: 'app/app.template.html'
 })
-export class AppComponent { }
+export class AppComponent { 
+	title: string = 'My Blog';
+	
+	navController: NavController = {
+		collapsed: false
+	}
+}
