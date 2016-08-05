cfa23dfe3866909bc8be57786ef4ef6902bb10b0
Sebastian Fiss <sebastianfiss@gmail.com>
2016-08-05 15:26:41 +0200

    fix app to support ng-bootstrap

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import {NGB_DIRECTIVES, NGB_PRECOMPILE} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
 	selector: 'blog-app',
+	precompile: [ NGB_PRECOMPILE ],
+	directives: [ NGB_DIRECTIVES ],
 	styleUrls: ['app/app.styles.css'],
 	templateUrl: 'app/app.template.html'
 })
--- systemjs.config.js
+++ systemjs.config.js
@@ -8,13 +8,15 @@
     'app':                        'app', // 'dist',
 
     '@angular':                   'node_modules/@angular',
+	'@ng-bootstrap':              'node_modules/@ng-bootstrap',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'app.bootstrap.js',  defaultExtension: 'js' },
+    'app':                        { main: 'app.bootstrap.js', defaultExtension: 'js' },
+	'@ng-bootstrap/ng-bootstrap': { main: 'index.js', defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
