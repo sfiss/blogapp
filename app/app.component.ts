94a6b6488f8c94d241228632533f4b21e5a24719
Sebastian Fiss <sebastianfiss@gmail.com>
2016-08-05 17:37:04 +0200

    refactor to nav component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,38 +1,22 @@
 import { Component } from '@angular/core';
 import {NGB_DIRECTIVES, NGB_PRECOMPILE, NGB_COLLAPSE_DIRECTIVES } from '@ng-bootstrap/ng-bootstrap';
-
-export class NavItem {
-	public name: string;
-	
-	constructor(name: string) {
-        this.name = name;
-    }
-}
-
-export class NavController {
-	public collapsed: boolean;
-	public items: Array<NavItem>;
-}
-
-const ITEMS: Array<NavItem> = [
-				new NavItem("Blog"),
-				new NavItem("About")
-			]
+import { NavItem, NavComponent } from './nav/nav.component';
 
 @Component({
 	selector: 'blog-app',
 	precompile: [ NGB_PRECOMPILE ],
-	directives: [ NGB_DIRECTIVES, NGB_COLLAPSE_DIRECTIVES ],
+	directives: [ NGB_DIRECTIVES, NGB_COLLAPSE_DIRECTIVES, NavComponent ],
 	styleUrls: ['app/app.styles.css'],
 	templateUrl: 'app/app.template.html'
 })
 export class AppComponent { 
 	public title: string = 'My Blog';
-	
-	navController: NavController = {
-		collapsed: true,
-		items: ITEMS
-	}
+	public navItems = NAVITEMS;
 }
+
+const NAVITEMS: Array<NavItem> = [
+				new NavItem("Blog"),
+				new NavItem("About")
+			]
 
 
---
+++ app/nav/nav.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import {NGB_DIRECTIVES, NGB_PRECOMPILE, NGB_COLLAPSE_DIRECTIVES } from '@ng-bootstrap/ng-bootstrap';
+
+export class NavItem {
+	public name: string;
+	
+	constructor(name: string) {
+        this.name = name;
+    }
+}
+
+@Component({
+	selector: 'blog-nav',
+	precompile: [ NGB_PRECOMPILE ],
+	directives: [ NGB_DIRECTIVES, NGB_COLLAPSE_DIRECTIVES ],
+	styleUrls: ['app/nav/nav.styles.css'],
+	templateUrl: 'app/nav/nav.template.html'
+})
+export class NavComponent {
+	collapsed: boolean = true;
+	
+	@Input()
+	title: string;
+	
+	@Input()
+	public items: Array<NavItem>;
+}
+
+
